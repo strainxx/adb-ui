1bfed22edcdadbfe884455e86e2f529e78eca25d
strainxx <yarik.chotkii@gmail.com>
2023-06-17 17:14:33 +0300

    ✅ adb downloader completed (not full)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const path = require('path');
-const {app, BrowserWindow, Menu, clipboard, ipcMain, Tray} = require('electron');
+const {app, BrowserWindow, Menu, clipboard, ipcMain, Tray, downloadURL, shell} = require('electron');
 const fs = require('fs')
 const settings = require('./settings.json')
+const {download} = require("electron-dl");
+const decompress = require("decompress");
 
 let win;
 
@@ -20,7 +22,13 @@ function createWindow() {
             titleBarStyle: 'hidden',
             icon: __dirname + "/img/icon.png"
     });
-    win.loadFile("installer.html");
+    if(settings.firstRun){
+        win.loadFile("installer.html");
+    } else {
+        win.loadFile("menu.html")
+    }
+    
+    fs.writeFileSync("lastest.log", "App started \n")
     
     //win.webContents.openDevTools();
 
@@ -46,17 +54,48 @@ app.whenReady().then(() => {
     if(!settings.tray){return false}
     tray = new Tray(__dirname + "/img/icon.png")
     const contextMenu = Menu.buildFromTemplate([
-        { label: 'Item1', type: 'radio' },
-        { label: 'Item2', type: 'radio' },
-        { label: 'Item3', type: 'radio', checked: true },
-        { label: 'Item4', type: 'radio' }
+        { label: 'Source Code', click: async () => {
+            const { shell } = require('electron')
+            await shell.openExternal('https://github.com/strainxx')
+            shell.beep()
+        }},
+        { label: 'split', type: 'separator' },
+        { label: 'Installation', type: 'radio', checked: true },
+        { label: 'Quit', click: async () => {
+            app.quit()
+        }}
     ])
     tray.setToolTip('ADB UI Tray')
     tray.setContextMenu(contextMenu)
 })
 
 ipcMain.on('closepls', () =>{
+    //fs.writeFileSync('./settings.json', JSON.stringify(settings));
     app.quit()
+})
+
+ipcMain.on('checkInstalled', (event, arg) => {
+    let adbExist = fs.existsSync('./platform_tools/adb.exe');
+    event.returnValue = adbExist;
+})
+
+ipcMain.on("download", (event, info) => {
+    download(BrowserWindow.getFocusedWindow(), info.url, info.properties)
+        .then(dl => event.returnValue = `Absolute path: ${dl.getSavePath()} <br> URL: ${info.url}<br>Succefully downloaded`)
+});
+
+ipcMain.on("unzip", (event, args) => {
+    decompress(args, "./")
+    .then((files) => {
+        event.returnValue = `Unziped ${args}`
+    })
+    .catch((error) => {
+        event.returnValue = error
+    });
+})
+
+ipcMain.on("cmd", (event, args) => {
+    shell.openExternal("cmd.exe")
 })
 
 console.log('Hello')
--- render.js
+++ render.js
@@ -1,6 +1,51 @@
-const { ipcRenderer } = require('electron')
+const { ipcRenderer } = require('electron');
+const settings = require('./settings.json');
+const fs = require('fs');
+const path = require('path');
+
+let log = "Install log:";
+const date = new Date
+
+function checkInstalled(){
+    let result = ipcRenderer.sendSync('checkInstalled');
+    if(!result){
+        alert("ADB not installed.")
+        document.getElementById("inslabel").style.visibility = "visible";
+        let dowloadRes = ipcRenderer.sendSync("download", 
+        {
+            url: "https://dl.google.com/android/repository/platform-tools-latest-windows.zip",
+            properties: {
+                directory: __dirname,
+                filename: "adb.zip"
+            }
+        });
+        log += `<br>${dowloadRes}`
+        document.getElementById("inslabel").innerHTML = log
+        let unzipRes = ipcRenderer.sendSync("unzip", "adb.zip")
+        log += `<br>Unzip log: ${unzipRes}<br>Deleting adb.zip<br>Saving log<br>`
+        document.getElementById("inslabel").innerHTML = log
+        fs.unlinkSync("adb.zip")
+        setTimeout(() => {
+            settings.firstRun = false;
+            fs.writeFileSync('./settings.json', JSON.stringify(settings));
+            document.getElementById("inslabel").innerHTML += `First run now is false`
+            location.href = "menu.html"
+        }, 3000);
+        fs.appendFileSync("lastest.log", log.replaceAll("<br>", `\n`))
+        
+    } else {
+        settings.firstRun = false;
+        location.href = "menu.html"
+    }
+}
 
 function closepls(info){
-    alert(info)
-    ipcRenderer.send('closepls')
+    alert(info);
+    ipcRenderer.send('closepls');
 }
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
